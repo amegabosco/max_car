86bd26c80787d8e9be968c87c716365404fdcd62
amegabosco <amegabosco@gmail.com>
2023-07-12 15:41:10 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,660 @@
+var dataGeo ={}
+dataGeo.bangui = {
+    "type": "Feature",
+    "properties": {
+        place:"bangui"
+    },
+    "geometry": {
+        "coordinates": [
+            [
+                [
+                    18.525737536745254,
+                    4.491987732628161
+                ],
+                [
+                    18.512166928085804,
+                    4.48928194285692
+                ],
+                [
+                    18.49782085607444,
+                    4.480004873228438
+                ],
+                [
+                    18.492004880935326,
+                    4.463769718327839
+                ],
+                [
+                    18.478434272275877,
+                    4.449467022370541
+                ],
+                [
+                    18.457109030097,
+                    4.4282057449251795
+                ],
+                [
+                    18.449354396577604,
+                    4.423566839457536
+                ],
+                [
+                    18.44547707981789,
+                    4.405397513567053
+                ],
+                [
+                    18.447803469873747,
+                    4.388000934488446
+                ],
+                [
+                    18.459823151828317,
+                    4.371763761617558
+                ],
+                [
+                    18.471455102108422,
+                    4.365578079347699
+                ],
+                [
+                    18.478434272275877,
+                    4.368670926859863
+                ],
+                [
+                    18.48502571076787,
+                    4.343927790389301
+                ],
+                [
+                    18.489678494537372,
+                    4.316090796727821
+                ],
+                [
+                    18.509840541688817,
+                    4.326143164290741
+                ],
+                [
+                    18.5385326857116,
+                    4.3149308995919995
+                ],
+                [
+                    18.546287319231,
+                    4.331555922352678
+                ],
+                [
+                    18.55326648939848,
+                    4.356299465244476
+                ],
+                [
+                    18.575367194929328,
+                    4.357072687867671
+                ],
+                [
+                    18.59514151040426,
+                    4.370217350704948
+                ],
+                [
+                    18.61181397247148,
+                    4.367511115407865
+                ],
+                [
+                    18.622670459398705,
+                    4.356299465244476
+                ],
+                [
+                    18.65136260342146,
+                    4.365578084219067
+                ],
+                [
+                    18.63895518979041,
+                    4.372923576232523
+                ],
+                [
+                    18.62422138610259,
+                    4.379882396978317
+                ],
+                [
+                    18.60871211906371,
+                    4.3864545571345985
+                ],
+                [
+                    18.602120680571744,
+                    4.398825525191924
+                ],
+                [
+                    18.58467275515258,
+                    4.403464585272729
+                ],
+                [
+                    18.58661141353295,
+                    4.4150621088623865
+                ],
+                [
+                    18.571489878169643,
+                    4.414675527659796
+                ],
+                [
+                    18.56916348811376,
+                    4.42897889787956
+                ],
+                [
+                    18.566061634706017,
+                    4.433617769411114
+                ],
+                [
+                    18.55210329437014,
+                    4.431298337283536
+                ],
+                [
+                    18.54706278258297,
+                    4.439802886148968
+                ],
+                [
+                    18.55249102604654,
+                    4.4486939006172435
+                ],
+                [
+                    18.545899587554544,
+                    4.451399840246992
+                ],
+                [
+                    18.53465536895095,
+                    4.468408375586677
+                ],
+                [
+                    18.525737536745254,
+                    4.491987732628161
+                ]
+            ]
+        ],
+        "type": "Polygon"
+    }
+}
+
+dataGeo.bangassou =  {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "coordinates": [
+            [
+                [
+                    22.810620678646444,
+                    4.763001951107398
+                ],
+                [
+                    22.791781944814602,
+                    4.743175258327142
+                ],
+                [
+                    22.81132493037839,
+                    4.713170951160819
+                ],
+                [
+                    22.852875782567565,
+                    4.717206693791567
+                ],
+                [
+                    22.859918299887653,
+                    4.772651904628006
+                ],
+                [
+                    22.82699453141575,
+                    4.771774641729166
+                ],
+                [
+                    22.810620678646444,
+                    4.763001951107398
+                ]
+            ]
+        ],
+        "type": "Polygon"
+    }
+}
+
+dataGeo.bria = {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "coordinates": [
+            [
+                [
+                    21.961850247325486,
+                    6.555490686529467
+                ],
+                [
+                    21.96020812125036,
+                    6.551369264576763
+                ],
+                [
+                    21.957701718294203,
+                    6.551884444183258
+                ],
+                [
+                    21.955368170715786,
+                    6.549823722564753
+                ],
+                [
+                    21.95528174302686,
+                    6.54767712849727
+                ],
+                [
+                    21.958306712111806,
+                    6.546303303449534
+                ],
+                [
+                    21.961504536573017,
+                    6.543984965110084
+                ],
+                [
+                    21.962973807270203,
+                    6.543984965110084
+                ],
+                [
+                    21.966085204042514,
+                    6.540893830584864
+                ],
+                [
+                    21.97464154516706,
+                    6.537888542576127
+                ],
+                [
+                    21.974555117479724,
+                    6.5364288247386355
+                ],
+                [
+                    21.978012225004477,
+                    6.533766975359995
+                ],
+                [
+                    21.977061520435967,
+                    6.531877912368543
+                ],
+                [
+                    21.980259344895558,
+                    6.527326958610601
+                ],
+                [
+                    21.982938603228092,
+                    6.529130171652298
+                ],
+                [
+                    21.985445006182687,
+                    6.532221378897319
+                ],
+                [
+                    21.991667799728845,
+                    6.521659675356688
+                ],
+                [
+                    21.996767033328638,
+                    6.521144464609705
+                ],
+                [
+                    21.99866844246725,
+                    6.522174885574088
+                ],
+                [
+                    22.001952694615795,
+                    6.522174885574088
+                ],
+                [
+                    22.008089060472997,
+                    6.521230333104327
+                ],
+                [
+                    22.00955833117027,
+                    6.524579192917912
+                ],
+                [
+                    22.007570494344378,
+                    6.527756295638142
+                ],
+                [
+                    22.004199814506933,
+                    6.53162031231723
+                ],
+                [
+                    22.001261273110828,
+                    6.530675777661344
+                ],
+                [
+                    21.999014153219804,
+                    6.53290831124761
+                ],
+                [
+                    21.99927343628329,
+                    6.535226700967058
+                ],
+                [
+                    22.000396996229654,
+                    6.536944019756135
+                ],
+                [
+                    21.995902756447435,
+                    6.538403736089478
+                ],
+                [
+                    21.995902756447435,
+                    6.5392623907651455
+                ],
+                [
+                    21.995297762629804,
+                    6.54518706781478
+                ],
+                [
+                    21.995211334942525,
+                    6.5500813132323685
+                ],
+                [
+                    21.98950710752507,
+                    6.554374471446991
+                ],
+                [
+                    21.98492644005401,
+                    6.556950348635851
+                ],
+                [
+                    21.98371645242031,
+                    6.552313760115595
+                ],
+                [
+                    21.981642187905493,
+                    6.549909586135428
+                ],
+                [
+                    21.978012225004477,
+                    6.547848856362393
+                ],
+                [
+                    21.973345129846052,
+                    6.548707494801846
+                ],
+                [
+                    21.9695423115688,
+                    6.552743075678407
+                ],
+                [
+                    21.961850247325486,
+                    6.555490686529467
+                ]
+            ]
+        ],
+        "type": "Polygon"
+    }
+}
+
+dataGeo.kaga =  {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "coordinates": [
+            [
+                [
+                    19.176811736358445,
+                    6.967494561877004
+                ],
+                [
+                    19.17561010671986,
+                    6.976866120585541
+                ],
+                [
+                    19.181103270781875,
+                    6.974139868297499
+                ],
+                [
+                    19.18384985281284,
+                    6.976099363733752
+                ],
+                [
+                    19.187540572417817,
+                    6.97788846110123
+                ],
+                [
+                    19.191488784087397,
+                    6.977036510826537
+                ],
+                [
+                    19.197325270903235,
+                    6.979847940848799
+                ],
+                [
+                    19.19809774710029,
+                    6.98615229827773
+                ],
+                [
+                    19.193720381987532,
+                    6.990497143789028
+                ],
+                [
+                    19.1996426994927,
+                    6.995353099785959
+                ],
+                [
+                    19.19878439260802,
+                    7.00804650052757
+                ],
+                [
+                    19.190115493071886,
+                    7.015117171202576
+                ],
+                [
+                    19.188828032744055,
+                    7.021506239109613
+                ],
+                [
+                    19.1874547417286,
+                    7.0290026335677
+                ],
+                [
+                    19.1731210167535,
+                    7.031898935409345
+                ],
+                [
+                    19.156555693878232,
+                    7.028406333888583
+                ],
+                [
+                    19.147200148834315,
+                    7.025680382755809
+                ],
+                [
+                    19.14127783132915,
+                    7.011198499506804
+                ],
+                [
+                    19.139990371001375,
+                    6.994927140753617
+                ],
+                [
+                    19.13029150320361,
+                    6.987089425235027
+                ],
+                [
+                    19.1409345085753,
+                    6.976610535107795
+                ],
+                [
+                    19.174751799835178,
+                    6.965449833255391
+                ],
+                [
+                    19.176811736358445,
+                    6.967494561877004
+                ]
+            ]
+        ],
+        "type": "Polygon"
+    }
+}
+
+var dataProject ={};
+dataProject.bangui  = `
+<p>Installation des PSA</p>
+<p>Aménagement de 02 sites pour centrales et générateurs</p>
+<p>Mise à niveau Électrique sur 02 Sites</p>
+<p>Aménagement de sites pour Installation des Incinérateurs (1)</p>
+<p>Réhabilitation bâtiment administratif de la DLMT: Bâtiment de type R+3 (1)</p>
+<p>Appuyer la réhabilitation des espaces de stockage des intrants - Entrepôt Central Delebama (Site PK24) axe Damara (1)</p>
+<p>Travaux UCM PAM: Entrepôt 6, Mise à niveau Électrique et Réhabilitation nouvel entrepôt (1)</p>
+<p>COVID-19- Aménagement des espaces de dépistage et de triage (5)</p>
+<p>Réhabilitation du centre TB-MR-Bangui (1)</p>
+<p>Aménagement des locaux des centres de santé carcéraux - site à identifier</p>
+`
+
+dataProject.bangassou =`
+
+    <p>
+    Réhabilitation des Centres et de traitement et de Dépistage de la Tuberculorse CT et CDT (3)
+    </p>
+
+`
+
+dataProject.berberati =`
+
+    <p></p>
+
+`
+dataProject.birao =`
+
+    <p></p>
+
+`
+
+dataProject.bria =`
+
+    <p>Réhabilitation des Centres et de traitement et de Dépistage de la Tuberculorse CT et CDT (2)</p>
+
+`
+
+dataProject.bouar =`
+
+    <p>Réhabilitation des Centres et de traitement et de Dépistage de la Tuberculorse CT et CDT (1)</p>
+
+    <p>Aménagement des locaux des centres de santé carcéraux (1)</p>
+
+`
+
+var config = {
+    style: 'mapbox://styles/amegabosco/cljyehhmf00ak01nmfkpta1zn',
+    accessToken: 'pk.eyJ1IjoiYW1lZ2Fib3NjbyIsImEiOiJ0SWE0WktNIn0.Dhoe8NU0itRsIY9iYL5b2A',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    title:  'SNAPSHOT of the CRF project Central African Republic Jul 2023',
+    //subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a UNOPS CAR dreamData team',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Display Title',
+            image: './images/Joy-wp.jpg',
+            description: 'zomloa',
+            location: {
+                center: [20.93841, 6.63949],
+                zoom: 5,
+                pitch: 80.00,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '<b>BANGUI (9)</b>',
+            image: './path/to/image/source.png',
+            description: dataProject.bangui ,
+            location: {
+                center: [18.54745, 4.40482],
+                zoom: 7,
+                pitch: 45.00,
+                bearing: 23.00,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'BANGASSOU (3)',
+            image: './path/to/image/source.png',
+            description: dataProject.bangassou,
+            location: {
+                center: [22.82330, 4.74353],
+                zoom: 9,
+                pitch: 30.00,
+                bearing: -15.51
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'right',
+            hidden: false,
+            title: 'BRIA (2)',
+            image: './path/to/image/source.png',
+            description: dataProject.bria,
+            location: {
+                center: [21.98718, 6.53927],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fith-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'BOUAR (2)',
+            image: './path/to/image/source.png',
+            description: dataProject.bouar,
+            location: {
+                center: [15.57934, 5.93969],
+                zoom: 5,
+                pitch: 0.00,
+                bearing: -4.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
